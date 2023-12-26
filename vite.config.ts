d9033aa0e7517eb9d1d3ba00da086db968a0ca96
曾佳俊 <952432141@qq.com>
2023-12-26 15:50:14 +0800

    chore: vite.config.ts 添加 base

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  publicDir: "/lowcode-gpt4",
+  base: "/lowcode-gpt4",
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
